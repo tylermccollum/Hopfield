e65f8aafebec922a1a94fdae9ea67ad60b91fd99
Tyler McCollum <tyler.c.mccollum@gmail.com>
2024-08-15 11:42:44 -0400

    Initial commit

--- tensor_dot.py
+++ tensor_dot.py
@@ -2,15 +2,16 @@ import numpy as np
 
 x, y, z = 2, 2, 3
 
-A = np.random.rand(x,y,z)
-B = np.random.rand(x,y)
+A = np.matrix('1 2; 3 4')
+B = np.matrix('1 2')
 
 print(f"Matrix A: {A}")
 print(f"Matrix B: {B}")
 
-B_reshaped = B[:, :, np.newaxis]
+# B_reshaped = B[:, :, np.newaxis]
 
-C = np.einsum('ijk,ij->ij', A, B)
+# C = np.einsum('ijk,ij->ij', A, B)
+C = A @ B.T
 
 print(f"Matrix C: {C}")
 
